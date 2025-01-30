58ef60ff0867ed9c972b2b5218c22ab9ee4d8d45
Stephen <stevobialor@gmail.com~>
2025-01-30 23:12:40 +0100

    update made

--- listings/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import PropertyViewSet, BookingViewSet
-
-router = DefaultRouter()
-router.register(r'properties', PropertyViewSet, basename='listing')
-router.register(r'bookings', BookingViewSet, basename='booking')
-
-urlpatterns = router.urls
