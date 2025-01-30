7639bc257d12526fda75f44f1df19febf5c160a8
Stephen <stevobialor@gmail.com~>
2025-01-30 23:19:39 +0100

    update made

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing
+from .serializers import ListingSerializer
+from .models import Booking
+from .serializers import BookingSerializer
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
